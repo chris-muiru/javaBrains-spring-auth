95d38b09863fab806914f39c8d05ec73143ea61c
kris <krisdjangodeveloper@gmail.com>
2023-02-02 12:52:10 +0300

    add:basic authorization

--- src/main/java/com/example/SpringSecurity/Config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurity/Config/SecurityConfig.java
@@ -1,26 +1,30 @@
 package com.example.SpringSecurity.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
+       /* auth.inMemoryAuthentication()
                 .withUser("kris")
                 .password("kris")
                 .roles("USER")
                 .and()
                 .withUser("james")
                 .password("james")
-                .roles("ADMIN");
-
+                .roles("ADMIN");*/
+        auth.userDetailsService(userDetailsService);
     }
     @Bean
     public PasswordEncoder getPasswordEncoder(){
@@ -31,15 +35,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
-                .antMatchers("/admin")
+                .antMatchers("/admin/**")
                 .hasRole("ADMIN")
                 .antMatchers("/user")
                 .hasAnyRole("USER","ADMIN")
                 .antMatchers("/").permitAll()
                 .and()
-                .formLogin();
+                .formLogin()
+                .and()
+                .httpBasic();
     }
 }
-// Authrntication manager does the authentication
-//Auth manager builder - configure Auth manager
+// Authentication manager does the authentication
+// Auth manager builder - configure Auth manager
 // 
