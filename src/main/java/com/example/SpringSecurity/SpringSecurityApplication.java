933f2e915265aacbad5ed227688bda0edd49c53d
kris <krisdjangodeveloper@gmail.com>
2023-02-02 12:51:39 +0300

    update: enable jpa repository to search for repository used by user service

--- src/main/java/com/example/SpringSecurity/SpringSecurityApplication.java
+++ src/main/java/com/example/SpringSecurity/SpringSecurityApplication.java
@@ -1,9 +1,12 @@
 package com.example.SpringSecurity;
 
+import com.example.SpringSecurity.Repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
